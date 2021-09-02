4036a0297ecd7c6e09354ea7bb59f4e61b2f8ea4
Teerasej <teerasej@gmail.com>
2021-09-02 22:38:29 +0700

    move homepage to its file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nextflow_flutter_battery_native/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,23 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Nextflow Battery Native'),
+      home: HomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
